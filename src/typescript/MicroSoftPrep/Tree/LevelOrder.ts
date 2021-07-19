f9620d60d12c00be13bee5774cd86b29ca1b3943
ravitejagGitHub <raviteja.giduturi@gmail.com>
2021-07-19 13:48:57 +0530

    tree level order 2

--- src/typescript/MicroSoftPrep/Tree/LevelOrder.ts
+++ src/typescript/MicroSoftPrep/Tree/LevelOrder.ts
@@ -20,3 +20,33 @@ export function levelOrder(root: TreeNode): number[] {
 
     return levelOrderArray;
 }
+
+export function levelOrder2(root: TreeNode): number[][] {
+    let queue: TreeNode[] = [];
+    let childNodes: TreeNode[] = [];
+    const levelOrderArray: number[][] = [];
+    let curr_node: TreeNode | null = root;
+    if (curr_node === null) {
+        return [];
+    }
+    queue.push(curr_node);
+    while (queue.length > 0) {
+        if (queue.length > 0) {
+            levelOrderArray.push(queue.map(v => v.val));
+        }
+        childNodes = [];
+
+        while (queue.length > 0) {
+            curr_node = queue.shift() as TreeNode;
+            if (curr_node.left !== null) {
+                childNodes.push(curr_node.left);
+            }
+            if (curr_node?.right !== null) {
+                childNodes.push(curr_node.right);
+            }
+        }
+        queue = childNodes;
+    }
+
+    return levelOrderArray;
+}
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,4 +1,5 @@
 import { printGUI, Tree } from "./lib/Tree";
-import { inOrder, levelOrder, postOrder, preOrder } from "./MicroSoftPrep/Tree";
+import { inOrder, levelOrder, levelOrder2, postOrder, preOrder } from "./MicroSoftPrep/Tree";
 printGUI()
 console.log(levelOrder(Tree));
+console.log(levelOrder2(Tree));

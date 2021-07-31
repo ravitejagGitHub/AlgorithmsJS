a2ba480b506b8d10916669f4865bcdc626b69f02
ravitejagGitHub <raviteja.giduturi@gmail.com>
2021-07-31 16:45:48 +0530

    removeDuplicatesFromSortArray

--- src/typescript/MicroSoftPrep/Arrays/RemoveDuplicatesfromSortedArray_26.ts
+++ src/typescript/MicroSoftPrep/Arrays/RemoveDuplicatesfromSortedArray_26.ts
@@ -1,11 +1,20 @@
 
 
 export function removeDuplicates(nums: number[]): number {
-    let index = 1;
-    for (let i = 1; i < nums.length; i++) {
-        if (nums[index - 1] !== nums[i]) {
-            nums[index++] =nums[i]
+    let index = 0;
+    for(let i = 1; i<nums.length; i++) {
+        if(nums[index]!== nums[i]) {
+            nums[++index] = nums[i]
         }
     }
     return index;
+};
+
+export function removeDuplicates2(nums: number[]): number {
+    let i;
+    const uniqueArray = Array.from(new Set(nums));
+    for(i = 0; i< uniqueArray.length; i++) {
+        nums[i]= uniqueArray[i];
+    }
+    return i;
 };
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -2,6 +2,7 @@ import { LinkedList, print } from "./lib/linked-list";
 import { printGUI, printTree, Tree } from "./lib/Tree";
 import { twoSum } from "./MicroSoftPrep/2Sum_1";
 import { mergeSortedArray } from "./MicroSoftPrep/Arrays/MergeSortedArrays";
+import { removeDuplicates } from "./MicroSoftPrep/Arrays/RemoveDuplicatesfromSortedArray_26";
 import { subarraySum } from "./MicroSoftPrep/Arrays/SubarraySumEqualsK_560";
 import { findDuplicate } from "./MicroSoftPrep/FindDuplicateNumber_287";
 import { mergeTwoLists } from "./MicroSoftPrep/LinkedList/MergeSortedList_21";
@@ -21,6 +22,8 @@ import { rightView } from "./MicroSoftPrep/Tree/RightView";
 
 // console.log(mergeSortedArray([1,4,6,7,12,18,30],[2,3,5,6,8,14,19]));
 
-let l1 = new LinkedList([1, 2, 3])
-let l2 = new LinkedList([11, 12, 13])
-print(mergeTwoLists(l1.head(), l2.head()));
+// let l1 = new LinkedList([1, 2, 3])
+// let l2 = new LinkedList([11, 12, 13])
+// print(mergeTwoLists(l1.head(), l2.head()));
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))

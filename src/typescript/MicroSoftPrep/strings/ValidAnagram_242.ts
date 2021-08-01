ef4f42bae9b8f1f5f31c6f3d49b57c5d971c87d8
ravitejagGitHub <raviteja.giduturi@gmail.com>
2021-08-01 16:21:33 +0530

    ValidAnagram_242

--- src/typescript/MicroSoftPrep/strings/ValidAnagram_242.ts
+++ src/typescript/MicroSoftPrep/strings/ValidAnagram_242.ts
@@ -1,30 +1,30 @@
 
-function isAnagram(s: string, t: string): boolean {
+function isAnagram1(s: string, t: string): boolean {
     if (s.length !== t.length) {
         return false;
     }
 
-    return s.split("").sort().join() === t.split("").sort().join();;
-
+    return s.split("").sort().join() === t.split("").sort().join();
 };
 
-function isAnagram2(s: string, t: string): boolean {
+
+export function isAnagram(s: string, t: string): boolean {
+    const map = new Map<string, number>();
+    let flag = true;
     if (s.length !== t.length) {
         return false;
     }
 
-    const countMap :{ [key:string]: number} = {};
-    
     for (let i = 0; i < s.length; i++) {
-        countMap[s[i]] = (countMap[s[i]] || 0) + 1;
-        countMap[t[i]] = (countMap[t[i]]|| 0 ) - 1;
+        map.set(s[i], (map.get(s[i]) || 0) + 1);
+        map.set(t[i], (map.get(t[i]) || 0) - 1);
     }
-    const mapArr = Object.values(countMap);
-    for (let val of mapArr) {
-        if (val !== 0) {
-            return false;
-        }
-    }
-    return true;
 
+    map.forEach(val => {
+        if (val !== 0) {
+            return flag = false;
+        }
+    })
+    return flag;
+    
 };
